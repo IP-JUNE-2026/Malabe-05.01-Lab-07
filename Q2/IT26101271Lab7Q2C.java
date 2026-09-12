15d721908110e6f2c7b1218135231dd59fa8df48
IT26101271 <IT26101271@my.sliit.lk>
2026-09-12 21:55:00 +0530

    IT26101271Lab7Q2C.java

---
+++ Q2/IT26101271Lab7Q2C.java
@@ -0,0 +1,12 @@
+public class IT26101271Lab7Q2C {
+
+    public static void main(String[] args) {
+
+        for (int row = 5; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(row);
+            }
+            System.out.println();
+        }
+    }
+}
