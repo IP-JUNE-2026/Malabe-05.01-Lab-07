7251a14161c30cf028b304b487e11e0c2fb137af
IT26101232 <it26101232@my.sliit.lk>
2026-09-12 11:32:42 +0530

    Add files via upload

---
+++ Q2/IT26101232Lab7Q2B.java
@@ -0,0 +1,21 @@
+public class IT26101232Lab7Q2B{
+
+   public static void main(String[] args){
+   
+    int r,c;
+	
+	for(r = 1; r <= 5; r++){
+		
+		System.out.print(r + " - ");
+		
+		for(c = 1; c <= r; c++){
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+        
+      
+   }
+
+
+}
